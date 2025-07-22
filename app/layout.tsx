97624d3a67e4dd631400c71c5aabcf625e9b88cb
omstephen <omstephen@lidowin.com>
2025-07-22 12:59:06 +0700

    fav icon

--- app/layout.tsx
+++ app/layout.tsx
@@ -14,6 +14,9 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
+      <head>
+        <link rel="icon" type="image/webp" href="/logo.webp" />
+      </head>
       <body>{children}</body>
     </html>
   )
